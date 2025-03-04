d081d49fa639b24d5cc7be2f570986ab96689d27
minhbro122 <Lynhan.zip.zip.zip@gmail.com>
2025-03-04 22:52:51 +0700

    Add files via upload

--- session20/bai2.js
+++ session20/bai2.js
@@ -1,23 +1,13 @@
-let string = prompt("Mời nhập chuỗi");
-let search = prompt("Mời nhập từ cần tìm kiếm");
-let found = false;
-
-for (let i = 0; i <= string.length - search.length; i++) {
-    let match = true;
-    for (let j = 0; j < search.length; j++) {
-        if(string[i+j] !== search[j]){
-            match = false;
-            break;
+let number = Number(prompt("Nhap vao 1 so nguyen"));
+if (Number.isInteger(number) && number > 0) {
+    let sum = "";
+    for (let i = 1; i <= number; i++) {
+        if (i % 5 === 0) {
+            sum += i + ", ";
         }
     }
-    if(match){
-        found = true;
-        break;
-    }
-}
-
-if(found){
-    alert('Tồn tại từ khóa tìm kiêm');
-}else{
-    alert('Không tồn tại từ khóa tìm kiếm');
+    sum = sum.slice(0, -2);
+    document.write(`Các số chia hết cho 5 từ 1 đến ${number} là: ${sum}`);
+} else {
+    alert("Dữ liệu nhập vào không hợp lệ. Vui lòng nhập một số nguyên dương.");
 }
